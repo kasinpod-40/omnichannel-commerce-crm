fc848e095e19dc0c9f0d26321daf7fd29e9c72e5
kasinpod-40 <kasinpod40@gmail.com>
2026-08-02 17:55:10 +0700

    test: expect TH-66 health

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -23,7 +23,7 @@ describe("Omnichannel Commerce CRM worker", () => {
         await expect(response.json()).resolves.toMatchObject({
             ok: true,
             service: "omnichannel-commerce-crm",
-            version: "material-dashboard-refresh-th-65",
+            version: "material-purchase-dedupe-th-66",
         });
     });
 
